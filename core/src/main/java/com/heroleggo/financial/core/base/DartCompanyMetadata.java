8ac531c420fd9bad8c987bd59e6497adf20a8fd2
Youngwoong You <h3r0.y0u@gmail.com>
2024-05-28 14:53:17 +0900

    chore: add server timezone on property

--- core/src/main/java/com/heroleggo/financial/core/base/DartCompanyMetadata.java
+++ core/src/main/java/com/heroleggo/financial/core/base/DartCompanyMetadata.java
@@ -1,15 +1,36 @@
 package com.heroleggo.financial.core.base;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Date;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@Entity(name = "DART_COMPANY_METADATA")
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
 public class DartCompanyMetadata {
+    @Id
+    @Column(name = "DART_COMPANY_METADATA_ID")
+    private Long id;
+
+    @Column(name = "CORPORATION_CODE", nullable = false)
     private String corporationCode;
+
+    @Column(name = "CORPORATION_NAME", nullable = false)
     private String corporationName;
+
+    @Column(name = "STOCK_CODE", nullable = true)
     private String stockCode;
+
+    @Column(name = "MODIFIED_AT")
     private Date modifiedAt;
 
     public static DartCompanyMetadata of(String corporationCode, String corporationName, String stockCode, Date modifiedAt) {
