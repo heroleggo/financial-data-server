53709fd3aa8f3fe16730d87da96f396fb16c5436
heroleggo <h3r0.y0u@gmail.com>
2024-06-08 16:14:17 +0900

    feat: add community domain entity

---
+++ core/src/main/java/com/heroleggo/financial/rest/board/entity/Post.java
@@ -0,0 +1,50 @@
+package com.heroleggo.financial.rest.board.entity;
+
+import com.heroleggo.financial.rest.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Post {
+    @Id
+    @Column(name = "post_id")
+    private Long id;
+
+    @Comment("")
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Comment("")
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Comment("생성일자")
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date createdAt;
+
+    @Comment("수정일자")
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private User user;
+}
--- core/src/main/java/com/heroleggo/financial/rest/board/entity/Reply.java
+++ core/src/main/java/com/heroleggo/financial/rest/board/entity/Reply.java
@@ -1,8 +1,11 @@
 package com.heroleggo.financial.rest.board.entity;
 
+import com.heroleggo.financial.rest.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.Date;
 import lombok.AllArgsConstructor;
@@ -12,21 +15,33 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
 @Entity
-@Table(name = "boards")
+@Table(name = "replies")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-public class Board {
+public class Reply {
     @Id
-    @Column(name = "board_id")
+    @Column(name = "reply_id")
     private Long id;
 
-    @Comment("게시판 명칭")
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Comment("댓글 본문")
+    @Column(name = "content", nullable = false)
+    private String content;
 
     @Comment("생성일자")
     @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Date createdAt;
+
+    @Comment("수정일자")
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
