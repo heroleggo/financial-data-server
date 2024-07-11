ad802cdebcbffa28e75e9a5ac428d6f4bb7a7df1
heroleggo <h3r0.y0u@gmail.com>
2024-07-11 21:52:06 +0900

    feat: add GICS industry

---
+++ core/src/main/java/com/heroleggo/financial/rest/company/entity/Industry.java
@@ -0,0 +1,41 @@
+package com.heroleggo.financial.rest.company.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "industries")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Industry {
+    @Id
+    @GeneratedValue
+    @Column(name = "industry_id")
+    private Long id;
+
+    @Comment("섹터명")
+    @Column(name = "sector")
+    private String sector;
+
+    @Comment("산업 그룹")
+    @Column(name = "industry_group")
+    private String industryGroup;
+
+    @Comment("산업 분류")
+    @Column(name = "industry")
+    private String industry;
+
+    @Comment("세부 산업 분류")
+    @Column(name = "sub_industry")
+    private String subIndustry;
+}
